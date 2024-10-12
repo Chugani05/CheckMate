aaef7d4cc6f2707d3c0b48bb5b4a4a8d9f413b5b
chugani05 <chugani205@gmail.com>
2024-10-12 23:42:21 +0100

    done??

--- todo/models.py
+++ todo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, max_length=1500)
+    slug = models.SlugField(unique=True, max_length=100)
     deadline = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     check_box = models.BooleanField(default=False)
